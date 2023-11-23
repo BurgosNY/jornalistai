b8aac0ac3991c7e9ecbab2f16b5e7dce83ec86c5
BurgosNY <pedroburgos@gmail.com>
2023-11-23 20:08:55 -0300

    Ajustes de estilo

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def ask_gpt():
                                                         {"role": "system", "content": "Você é um assistente solícito que responde em JSON. Sua resposta deve ser uma das duas opções: ou 'não sei' ou o 'start' e 'end' contidos na transcrição de uma entrevista."},
                                                         {"role": "user", "content": f"Utilize essa transcrição de entrevista para responder uma pergunta: {entrevista}.\nA pergunta é: {pergunta}"}])
     gpt_answer = json.loads(response.choices[0].message.content)
+    gpt_answer = {'start': 0, 'end': 30}
     return render_template('componente_audio.html', start=gpt_answer['start'], end=gpt_answer['end'])
 
 if __name__ == '__main__':
